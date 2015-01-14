2823d8878023d8c44296c6c9daae56b5b0fec0e6
Nick Arduini <nickarduini@gmail.com>
2015-01-14 11:25:12 -0800

    Mostly uncommented at this point.

--- gcd/main.c
+++ gcd/main.c
@@ -6,10 +6,121 @@
 //  Copyright (c) 2015 Nicholas Arduini. All rights reserved.
 //
 
-#include <stdio.h>
+/*
+ You are given an array A of integers of size N. You will be given Q queries where each query is represented by two integers L, R. You have to find the gcd(Greatest Common Divisor) of the array after excluding the part from range L to R inclusive (1 Based indexing). You are guaranteed that after excluding the part of the array
+ remaining array is non empty.
+ Input
+ 
+ First line of input contains an integer T denoting number of test cases.
+ For each test case, first line will contain two space separated integers N, Q.
+ Next line contains N space separated integers denoting array A.
+ For next Q lines, each line will contain a query denoted by two space separated integers L, R.
+ Output
+ 
+ For each query, print a single integer representing the answer of that query.
+ Constraints
+ 
+ Subtask #1: 40 points
+ 2 ≤ T, N ≤ 100, 1 ≤ Q ≤ N, 1 ≤ A[i] ≤ 105
+ 1 ≤ L, R ≤ N and L ≤ R
+ Subtask #2: 60 points
+ 2 ≤ T, N ≤ 105, 1 ≤ Q ≤ N, 1 ≤ A[i] ≤ 105
+ 1 ≤ L, R ≤ N and L ≤ R
+ Sum of N over all the test cases will be less than or equal to 106.
+ Example
+ 
+ Input:
+ 1
+ 3 3
+ 2 6 9
+ 1 1
+ 2 2
+ 2 3
+ 
+ Output:
+ 3
+ 1
+ 2
+ Explanation
+ 
+ For first query, the remaining part of array will be (6, 9), so answer is 3.
+ For second query, the remaining part of array will be (2, 9), so answer is 1.
+ For third query, the remaining part of array will be (2), so answer is 2.
+ Warning : Large IO(input output), please use faster method for IO.
+ */
 
-int main(int argc, const char * argv[]) {
+#include <stdio.h>
+#define gc getchar_unlocked
+
+static inline void scanint(int *x)
+{
+    register int c = gc();
+    *x = 0;
+    for(;(c<48 || c>57);c = gc());
+    for(;c>47 && c<58;c = gc()) {*x = (*x<<1) + (*x<<3) + c - 48;}
+}
+
+static inline void fast_output(int x)
+{
+    char a[20];
+    long long int i=0,j;
+    a[0]='0';
+    if (x<0) {putchar('-'); x=-x;}
+    if (x==0) putchar('0');
+    while(x)
+    {
+        a[i++]=x%10+48;
+        x/=10;
+    }
+    for(j=i-1;j>=0;--j)
+    {
+        putchar(a[j]);
+    }
+    putchar('\n');
+}
+
+static inline unsigned int gcd(unsigned int u, unsigned int v)
+{
+    int shift;
+    if (u == 0) return v;
+    if (v == 0) return u;
+    shift = __builtin_ctz(u | v);
+    u >>= __builtin_ctz(u);
+    do {
+        v >>= __builtin_ctz(v);
+        if (u > v) {
+            unsigned int t = v;
+            v = u;
+            u = t;
+        }
+        v = v - u;
+    } while (v != 0);
+    return u << shift;
+}
+
+int main(void) {
     // insert code here...
-    printf("Hello, World!\n");
+    int cases, size, queries, left, right, denom, arr[100000], front[100000], back[100000];
+    scanint(&cases);
+    int i, j, k;
+    for (i = 0; i < cases; ++i) {
+        scanint(&size);
+        scanint(&queries);
+        for (j = 0; j < size; ++j) {
+            scanint(&arr[j]);
+        }
+        front[0] = 0;
+        back[size] = 0;
+        for (j = 1; j <= size; ++j) {
+            front[j] = gcd(front[j-1], arr[j - 1]);
+            back[size - j] = gcd(back[size - j + 1], arr[size - j]);
+        }
+        for (k = 0; k < queries; ++k) {
+            scanint(&left);
+            scanint(&right);
+            denom = gcd(front[left - 1], back[right]);
+            fast_output(denom);
+        }
+    }
     return 0;
 }
